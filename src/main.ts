ff49f7c88720b3ee38a5d1f9dc6bc8402675b7a0
Yevhen Synytsia <yevhensynytsia@Yevhens-MacBook-Pro.local>
2022-05-29 00:32:36 +0300

    add env var

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
+import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+const Port = process.env.PORT || 4888;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(Port);
+  console.log(`App started in a Port ${Port}`);
+  console.log(process.env.PORT);
 }
 bootstrap();
